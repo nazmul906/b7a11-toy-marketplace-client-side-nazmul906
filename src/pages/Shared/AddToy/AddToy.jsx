639ff5165539a70178b897fc0906b2aba1fc25fb
finless88 <nazmul96@gmail.com>
2023-05-20 01:24:12 +0600

    sent add toy form data to backend using POST method

--- src/pages/Shared/AddToy/AddToy.jsx
+++ src/pages/Shared/AddToy/AddToy.jsx
@@ -1,16 +1,58 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AuthContext } from "../../../AuthProvider/AuthProvider";
 
 const AddToy = () => {
+  const { user } = useContext(AuthContext);
   const handleAddtoyForm = (event) => {
     event.preventDefault();
     const form = event.target;
     const photo = form.photo.value;
     const toyname = form.name.value;
-    const sellername = form.seller.value;
+    const sellername = user?.displayName;
+
+    const email = user?.email;
     const subcategory = form.subcategory.value;
+    const price = form.price.value;
+    const rating = form.rating.value;
+    const quantity = form.quantity.value;
+    const description = form.description.value;
 
     // const toy={}
-    console.log(photo, toyname, sellername, subcategory);
+    const toydata = {
+      pictureURL: photo,
+      name: toyname,
+      sellerName: sellername,
+      subcategory: subcategory,
+      price: price,
+      rating: rating,
+      quantity: quantity,
+      description: description,
+      sellerEmail: email,
+    };
+    console.log(toydata);
+
+    // fetch("http://localhost:5000/addtoy", {
+    //   method: "POST",
+    //   headers: { "content-type": "application/json" },
+    //   body: JSON.stringify(toydata),
+    // })
+    //   .then((res) => res.json())
+    //   .then((data) => console.log(data));
+
+    fetch("http://localhost:5000/addtoy", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(toydata),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.insertedId) {
+          alert("service book successfully");
+        }
+      });
   };
   return (
     <div>
@@ -22,6 +64,7 @@ const AddToy = () => {
             className="input input-bordered w-full"
             type="text"
             name="photo"
+            required
           />
         </div>
         <div className="block font-medium">
@@ -29,42 +72,70 @@ const AddToy = () => {
           <input
             type="name"
             name="name"
+            defaultValue={user?.displayName}
             className="input input-bordered w-full"
           />
         </div>
-        <div>
-          <label>Seller Name</label>
+        <div className="block font-medium">
+          <label>email</label>
           <input
-            type="sellername"
-            name="seller"
+            type="email"
+            name="email"
+            defaultValue={user?.email}
             className="input input-bordered w-full"
+            required
           />
         </div>
+
         <div>
           <label>Subcategory</label>
-          <select name="subcategory" className="select select-bordered w-full">
+          <select
+            name="subcategory"
+            className="select select-bordered w-full"
+            required
+          >
             <option value="">Select a sub-category</option>
             <option value="princess">Princess</option>
             <option value="frozendolls">Frozen Dolls</option>
             <option value="animation">Animation</option>
           </select>
         </div>
-        {/* <div>
-          <label></label>
-          <input />
+        <div>
+          <label>Price</label>
+          <input
+            type="price"
+            name="price"
+            className="input input-bordered w-full"
+            required
+          />
         </div>
         <div>
-          <label></label>
-          <input />
+          <label>Rating</label>
+          <input
+            type="rating"
+            name="rating"
+            className="input input-bordered w-full"
+            required
+          />
         </div>
         <div>
-          <label></label>
-          <input />
+          <label>Available quantity</label>
+          <input
+            type="quantity"
+            name="quantity"
+            className="input input-bordered w-full"
+            required
+          />
         </div>
         <div>
-          <label></label>
-          <input />
-        </div> */}
+          <label>Detail description</label>
+          <input
+            type="description"
+            name="description"
+            className="input input-bordered w-full"
+            required
+          />
+        </div>
         <div>
           <input type="submit" className="btn btn-primary" value="submit" />
         </div>
