b8ccd58b81de8c14f09f38f986b226d2d06c1c89
finless88 <nazmul96@gmail.com>
2023-05-20 03:42:34 +0600

    my toy info are shown as tabular form

--- src/pages/Shared/MyToy/MyToy.jsx
+++ src/pages/Shared/MyToy/MyToy.jsx
@@ -12,11 +12,49 @@ const MyToy = () => {
       .then((data) => {
         setMytoy(data);
       });
-  }, [url]);
+  }, [user]);
   return (
     <div>
-      <h4>{user.email}</h4>
-      {mytoy.length}
+      {/* <h4>{user.email}</h4>
+      {mytoy.length} */}
+      <div className="overflow-x-auto w-full">
+        <table className="table w-full">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>photo</th>
+              <th>Name</th>
+              <th>seller name</th>
+              <th>seller email </th>
+              <th>Price</th>
+              <th>Rating</th>
+              <th>Available quantity</th>
+              <th>Detail description</th>
+            </tr>
+          </thead>
+          <tbody>
+            {mytoy?.map((item, index) => (
+              <tr>
+                <td>{index + 1}</td>
+                <td>{item.pictureURL}</td>
+                <td>{item.name}</td>
+                <td>{item.email}</td>
+                <td>{item.sellerName}</td>
+                <td>{item.price}</td>
+                <td>{item.rating}</td>
+                <td>{item.description}</td>
+
+                <td>
+                  <button className="btn btn-primary">update</button>
+                </td>
+                <td>
+                  <button className="btn btn-primary">delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
